633a5fe8df2bae55f4fc5d3e177177cb5091509c
NonokEE <shshrdl@naver.com>
2022-05-07 16:05:25 +0900

    shuffle

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
 import numpy as np
+
+a = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9],
+              [1,5,9]])
+test = []
+
+newa = a.copy()
+np.random.shuffle(newa)
+test.append(newa[0])
+
+newa = a.copy()
+np.random.shuffle(newa)
+test.append(newa[0])
+newa = a.copy()
+np.random.shuffle(newa)
+test.append(newa[0])
+newa = a.copy()
+np.random.shuffle(newa)
+test.append(newa[0])
+newa = a.copy()
+np.random.shuffle(newa)
+test.append(newa[0])
+
+
+print(test)
